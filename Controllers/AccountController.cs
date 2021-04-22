b126491dabc875f2daaea72ae825574544e8dac1
Maruf Abdullaev <m.abdullaev4188@gmail.com>
2021-04-22 12:05:33 -0400

    Add register functionality

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -13,10 +13,12 @@ namespace FinalProject.Controllers
     public class AccountController : Controller
     {
         private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public AccountController(SignInManager<ApplicationUser> signInManager)
+        public AccountController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
             this.signInManager = signInManager;
+            this.userManager = userManager;
         }
 
         [HttpGet]
@@ -40,6 +42,48 @@ namespace FinalProject.Controllers
             }
 
             ModelState.AddModelError("Signin", "Login or password incorrect");
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(SignUpViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new ApplicationUser
+            {
+                DOB = model.DOB,
+                Email = model.Email,
+                FirstName = model.FirstName,
+                Gender = model.Gender,
+                LastName = model.LastName,
+                MiddleName = model.MiddleName,
+                PhoneNumber = model.PhoneNumber,
+                UserName = model.Login
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if(result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(user, "User");
+                return RedirectToAction("Login");
+            }
+
+            foreach(var error in result.Errors)
+            {
+                ModelState.AddModelError(error.Code, error.Description);
+            }
 
             return View(model);
         }
---
+++ ViewModels/SignUpViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.ViewModels
+{
+    public class SignUpViewModel
+    {
+        [Required]
+        public string Login { get; set; }
+        [MinLength(4)]
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public DateTime DOB { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public char Gender { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Phone]
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
